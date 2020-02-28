c8b34a12b1d9be97607e61cbf98d8d6541747a35
wangyiwen1987 <59366762+wangyiwen1987@users.noreply.github.com>
2020-02-28 22:10:50 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,7 +3,7 @@
 # Description: DIY script
 # Lisence: MIT
 # Author: P3TERX
-# Blog: https://p3terx.com
+# Blog: https://www.wangyiwennas.cn
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
